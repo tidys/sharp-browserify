86d60a3575cedc602648ddb81172c095e4303d0e
xu_yanfeng <xu_yanfeng@126.com>
2024-04-23 19:35:19 +0800

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+function sharp(arr) {
+    // console.log(`Don't call me, I'm just an empty shell`);
+}
+sharp.prototype.png = function () { }
+sharp.prototype.jpeg = function () { }
+module.exports = sharp;
